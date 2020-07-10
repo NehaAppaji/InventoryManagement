5039940522be77dc8aea2e9d011d930555d382ca
NehaAppaji <66670448+NehaAppaji@users.noreply.github.com>
2020-07-10 20:08:06 +0530

    Update Constants.java

--- Inventory/src/beans/Constants.java
+++ Inventory/src/beans/Constants.java
@@ -5,7 +5,7 @@ public class Constants {
 public static String DRIVER="com.mysql.jdbc.Driver";
 public static String URL="jdbc:mysql://localhost/inventory";
 public static String DB_USERNAME ="root";
-public static String DB_PASSWORD="root";
+public static String DB_PASSWORD="7777";
 public static int NO_ERROR=0;
 public static int INVALID_PASSWORD_ERROR=2;
 public static int INVALID_MAILID_ERROR=1;
